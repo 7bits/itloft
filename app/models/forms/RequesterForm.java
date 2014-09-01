baa8ee199276d8d4d51e3b35740553cb04018e01
Dmitry Shmatko <dmitry.shmatko@7bits.it>
2014-09-01 13:33:57 +0700

    Validators added for image and date

--- app/models/forms/RequesterForm.java
+++ app/models/forms/RequesterForm.java
@@ -1,14 +1,15 @@
 package models.forms;
 
-import play.data.binding.As;
-import play.data.validation.CheckWith;
 import play.data.validation.Email;
 import play.data.validation.InFuture;
 import play.data.validation.Phone;
 import play.data.validation.Required;
 import play.data.validation.URL;
 import play.db.jpa.Model;
-import utils.validators.DateAfterDateCheck;
+import utils.validators.DateAfter;
+import utils.validators.FileType;
+import utils.validators.ImageSize;
+import utils.validators.ImageTransparency;
 
 import java.io.File;
 import java.util.Date;
@@ -38,13 +39,16 @@ public class RequesterForm extends Model {
 
     @Required
     @InFuture
-    @CheckWith(DateAfterDateCheck.class)
+    @DateAfter(message = "validation.before", before = "startDate")
     public Date endDate;
 
     @Required
     public String description;
 
     @Required
+    @FileType(message = "validation.not-png-or-gif", types = { "image/jpeg", "image/png" })
+    @ImageSize(message = "validation.image-size-100-100", width = 100, height = 100)
+    @ImageTransparency
     public File logo;
 
     @Required
---
+++ app/utils/validators/impl/DateAfterCheck.java
@@ -0,0 +1,85 @@
+package utils.validators.impl;
+
+import net.sf.oval.Validator;
+import net.sf.oval.configuration.annotation.AbstractAnnotationCheck;
+import net.sf.oval.context.OValContext;
+import net.sf.oval.exception.OValException;
+import utils.validators.DateAfter;
+
+import java.lang.reflect.Field;
+import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Annotation {@link utils.validators.DateAfter} implementation
+ * Checks if date is after certain date
+ */
+public class DateAfterCheck extends AbstractAnnotationCheck<DateAfter> {
+
+    /** Error message key. */
+    public final static String message = "validation.date-after";
+
+    /** Date that should be before this date */
+    private String before;
+
+    /**
+     * Reads input parameters
+     * @param dateAfter annotation
+     */
+    @Override
+    public void configure(final DateAfter dateAfter) {
+        setMessage(dateAfter.message());
+        this.before = dateAfter.before();
+    }
+
+    /***
+     * Puts message variables in Map
+     * @return map with message variable: before field name
+     */
+    @Override
+    protected Map<String, ?> createMessageVariables() {
+        final Map<String, String> variables = new TreeMap<String, String>();
+        variables.put("2", before);
+        return variables;
+    }
+
+    /**
+     * Validates file type
+     * @param validatedObject    Validated object
+     * @param value              File
+     * @param oValContext
+     * @param validator
+     * @return true if date passes validation
+     * @throws net.sf.oval.exception.OValException
+     */
+    @Override
+    public boolean isSatisfied(
+            final Object validatedObject,
+            final Object value,
+            final OValContext oValContext,
+            final Validator validator
+    ) throws OValException {
+
+        requireMessageVariablesRecreation();
+        if (before == null) {
+            return true;
+        }
+
+        final Date beforeDate;
+        final Date thisDate = (Date) value;
+        try {
+            Class clazz = validatedObject.getClass();
+            Field field = clazz.getField(before);
+            beforeDate = (Date) field.get(validatedObject);
+        } catch (Exception e) {
+            return false;
+        }
+
+        if (thisDate != null && beforeDate != null) {
+            return thisDate.after(beforeDate);
+        } else {
+            return false;
+        }
+    }
+}
